f7b560c46b97c6e190f040a76d777079c04a6e20
Elfre Valdes <elfre.valdes@gmail.com>
2024-05-05 12:27:16 -0600

    BubbleSort inplementation

--- CodeChallange/CodeChallange/Program.cs
+++ CodeChallange/CodeChallange/Program.cs
@@ -1,7 +1,48 @@
-﻿public class Program
+﻿using System;
+
+public class BubbleSort
 {
-    public static void Main()
+    public static void BubbleSortArray(int[] arr)
     {
-        Console.WriteLine("Hello World");
+        int n = arr.Length - 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            int nMinOne = n - 1;
+            for (int j = 0; j < nMinOne - i; j++)
+            {
+                int jPlusOne = j + 1;
+                if (arr[j] > arr[jPlusOne])
+                {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[jPlusOne];
+                    arr[jPlusOne] = temp;
+                }
+            }
+        }
+        // Times it run 21, computations: 64
+        // Times it run 21, computations: 22
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+        Console.WriteLine("Original array:");
+        PrintArray(array);
+
+        BubbleSortArray(array);
+
+        Console.WriteLine("Array after Bubble Sort:");
+        PrintArray(array);
+    }
+
+    public static void PrintArray(int[] arr)
+    {
+        foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
     }
 }
