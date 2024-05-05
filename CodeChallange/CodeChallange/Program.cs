0648f331c61a9aaaa21acc1f19fd5c14823a9a0f
Elfre Valdes <elfre.valdes@gmail.com>
2024-05-05 12:29:17 -0600

    Get Nth Fibonacci

--- CodeChallange/CodeChallange/Program.cs
+++ CodeChallange/CodeChallange/Program.cs
@@ -1,7 +1,23 @@
-﻿public class Program
+﻿using System;
+
+public class Program
 {
-    public static void Main()
+    public static int GetNthFibonacci(int n)
     {
-        Console.WriteLine("Hello World");
+        if (n <= 0)
+            throw new ArgumentException("Input must be a positive integer.", nameof(n));
+
+        else if (n == 1 || n == 2)
+            return 1;
+
+        else
+            return GetNthFibonacci(n - 1) + GetNthFibonacci(n - 2);
+    }
+
+    public static void Main(string[] args)
+    {
+        int n = 10;
+        int result = GetNthFibonacci(n);
+        Console.WriteLine($"The {n}th Fibonacci number is: {result}");
     }
 }
