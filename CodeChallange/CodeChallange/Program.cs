7fb01a1d3997e2dadca9256d1a839e371d25d1cd
Elfre Valdes <elfre.valdes@gmail.com>
2024-05-05 12:25:04 -0600

    BST inplementation

--- CodeChallange/CodeChallange/Program.cs
+++ CodeChallange/CodeChallange/Program.cs
@@ -1,7 +1,91 @@
-﻿public class Program
+﻿using System;
+
+public class Program
 {
-    public static void Main()
+    public class BinarySearchTree
     {
-        Console.WriteLine("Hello World");
+
+        private class Node
+        {
+            public int data;
+            public Node left;
+            public Node right;
+
+            public Node(int data)
+            {
+                this.data = data;
+                left = null;
+                right = null;
+            }
+        }
+
+        private Node root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public void Insert(int data)
+        {
+            root = InsertRec(root, data);
+        }
+
+        private Node InsertRec(Node root, int data)
+        {
+            if (root == null)
+            {
+                root = new Node(data);
+                return root;
+            }
+
+            if (data < root.data)
+            {
+                root.left = InsertRec(root.left, data);
+            }
+            else if (data > root.data)
+            {
+                root.right = InsertRec(root.right, data);
+            }
+
+            return root;
+        }
+
+        public void InorderTraversal()
+        {
+            InorderTraversalRec(root);
+            Console.WriteLine();
+        }
+
+        private void InorderTraversalRec(Node root)
+        {
+            if (root != null)
+            {
+                // Go to the farthest left side
+                InorderTraversalRec(root.left);
+                // Middle
+                Console.Write(root.data + " ");
+                // Lets go right now
+                InorderTraversalRec(root.right);
+            }
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        BinarySearchTree bst = new BinarySearchTree();
+
+        bst.Insert(50);
+        bst.Insert(30);
+        bst.Insert(20);
+        bst.Insert(25);
+        bst.Insert(40);
+        bst.Insert(70);
+        bst.Insert(92);
+        bst.Insert(60);
+        bst.Insert(80);
+
+        Console.WriteLine("Inorder Traversal of Binary Search Tree:");
+        bst.InorderTraversal();
     }
 }
