c225bb2d74f1dc4456c0357e44af69d2b7ee7ade
Elfre Valdes <elfre.valdes@gmail.com>
2024-05-09 10:54:34 -0600

    Implements cashing answers

--- CodeChallange/CodeChallange/Program.cs
+++ CodeChallange/CodeChallange/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 
-public class Program
+public class Solution
 {
-    public static int GetNthFibonacci(int n)
+    Dictionary<int, int> Solved;
+
+    public Solution()
     {
+        Solved = new Dictionary<int, int>() { {0, 0}, {1, 1} };
+    }
+
+    public int GetNthFibonacci(int n)
+    { // O(2n)
         if (n <= 0)
             throw new ArgumentException("Input must be a positive integer.", nameof(n));
 
@@ -14,10 +21,36 @@ public class Program
             return GetNthFibonacci(n - 1) + GetNthFibonacci(n - 2);
     }
 
+    public int GetNthFibonacciCashed(int n)
+    {
+        if (Solved.ContainsKey(n))
+        {
+             return Solved[n];
+        }
+        else
+        {
+            for (int i = Solved.Count; i <= n; i++) 
+            {
+                Solved.Add(i, Solved[i - 1] + Solved[i - 2]);
+            }
+            return Solved[n];
+        }
+    }
+}
+public class Program
+{
     public static void Main(string[] args)
     {
         int n = 10;
-        int result = GetNthFibonacci(n);
+        Solution solver = new Solution();
+        // With recursion
+        int result = solver.GetNthFibonacci(n);
+        Console.WriteLine($"The {n}th Fibonacci number is: {result}");
+        // Linial
+        result = solver.GetNthFibonacciCashed(n);
+        Console.WriteLine($"The {n}th Fibonacci number is: {result}");
+        // cashed
+        result = solver.GetNthFibonacciCashed(n-1);
         Console.WriteLine($"The {n}th Fibonacci number is: {result}");
     }
 }
