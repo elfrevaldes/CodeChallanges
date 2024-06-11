9f5c59b805e617764ee72cfdbf362df0c9c6b0c3
Elfre Valdes <elfre.valdes@gmail.com>
2024-06-11 16:02:04 -0600

    Implements merging intervals

--- CodeChallenge/CodeChallenge/Program.cs
+++ CodeChallenge/CodeChallenge/Program.cs
@@ -1,12 +1,40 @@
 ﻿public class Solution
-{   
+{
+    public static List<(int, int)> MergeIntervals(List<(int, int)> intervals)
+    {
+        int start = intervals[0].Item1;
+        int end = intervals[0].Item2;
+        List <(int, int)> points = new List <(int, int)> ();
         
+        for (int i = 1; i < intervals.Count; i++)
+        {
+            if (end >= intervals[i].Item1)
+            {
+                end = intervals[i].Item2;
+            }
+            else
+            {
+                // save that one and began a new one
+                points.Add((start, end));
+                start = intervals[i].Item1;
+                end = intervals[i].Item2;
+            }
+            if (i + 1 == intervals.Count)
+                points.Add((start, end));
+        };
+        
+        return points;
+    }
+
 }
 
 public class Program
 {
     public static void Main()
-    {     
-        Console.WriteLine("Hello World");
+    {
+        List<(int, int)> stringList = new List<(int, int)>{ (1,3), (2,6), (6,9), (10,13), (15,18), (16, 24) };
+
+        List<(int, int)> solution = Solution.MergeIntervals(stringList);
+        Console.WriteLine(string.Join(", ", solution.Select(x => $"[{x.Item1}, {x.Item2}]")));
     }
 }
